e68188be79e2e43d02629b4a3e0dc2cd8eddd1aa
Robert Östling <robert@ling.su.se>
2022-11-30 17:19:02 +0100

    R prototype -- not sure this track will continue

---
+++ extract.R
@@ -0,0 +1,20 @@
+# apt install libgdal-dev libcurl4-openssl-dev libssl-dev
+# install.packages("lingtypology")
+
+# https://github.com/ropensci/lingtypology
+# https://ropensci.github.io/lingtypology/lingtypology_db_API.html
+# https://agricolamz.github.io/2018_Areal_Patterns/1_day.html
+# https://github.com/autotyp/autotyp-data
+
+library(lingtypology)
+
+df <- wals.feature(c('81a', '82a', '83a'))
+
+# Learn more R ...
+#df_macroarea <- area.lang(df$language)
+#merge(df, df_macroarea, by="Area")
+
+write.table(df, "wals.tsv", fileEncoding="UTF-8", sep="\t")
+
+
+
--- wals.py
+++ wals.py
@@ -1,19 +1,32 @@
-import urllib.request
-import zipfile
+from pycldf import Dataset
 
-class WALSData:
-    def __init__(self):
-        pass
+wals2020 = Dataset.from_metadata('https://raw.githubusercontent.com/cldf-datasets/wals/v2020/cldf/StructureDataset-metadata.json')
 
-    def get_filename(self):
-        url = 'https://zenodo.org/record/6806407/files/cldf-datasets/' \
-              'wals-v2020.2.zip?download=1'
-        filename = 'wals.zip'
-        if not os.path.exists(filename):
-            urllib.request.urlretrieve(url, filename)
-        return filename
+for value in wals2020.objects('ValueTable'):
+    print(value.parameter.__dict__.keys())
+    print(value.language.name, value.data['Parameter_ID'],
+            value.data['Value'])
+    break
 
-    # TODO: create methods to load the data, and some to produce simplified
-    # versions. May want to create one class per output format, e.g. JSON,
-    # Python code, s-expressions, etc.
+#import urllib.request
+#import zipfile
+#class WALSData:
+#    def __init__(self):
+#        pass
+#
+#    def get_file(self):
+#        """Return the filename of the WALS zip file, download if needed"""
+#        url = 'https://zenodo.org/record/6806407/files/cldf-datasets/' \
+#              'wals-v2020.2.zip?download=1'
+#        filename = 'wals.zip'
+#        if not os.path.exists(filename):
+#            urllib.request.urlretrieve(url, filename)
+#        return filename
+#
+#    def load_data(self):
+#        filename = self.get_file()
+#        prefix = ['cldf-datasets-wals-ba04a3a', 'raw']
+#    # TODO: create methods to load the data, and some to produce simplified
+#    # versions. May want to create one class per output format, e.g. JSON,
+#    # Python code, s-expressions, etc.
 
